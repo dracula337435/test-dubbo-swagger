4a75b217ec5caef9972bffb2f9aa55a49032c2de
dk <dracula337435@163.com>
2019-03-11 11:36:42 +0800

    一个可用的swagger

---
+++ src/main/java/io/dracula/test/dubbo/swagger/provider/SwaggerConfiguration.java
@@ -0,0 +1,13 @@
+package io.dracula.test.dubbo.swagger.provider;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author dk
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+}
